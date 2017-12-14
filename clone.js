677a5dc80bb2814ebfe164d85dbc067813c5da97
yangkuo <734271234@qq.com>
2017-12-14 13:17:30 +0800

    demo

--- clone.js
+++ clone.js
@@ -30,3 +30,44 @@ function clone(target) {
             return target
     }
 }
+/*
+* 数组去重
+* @param {Array}
+* */
+Array.prototype.norepeat = function () {
+    var empty = [];
+    for(var i = 0; i < this.length; i++) {
+        empty.indexOf(this[i]) === -1 ? empty.push(this[i]) : ''
+    }
+    return empty
+}
+function norepeat(array) {
+    return Array.from(new Set(array))
+}
+/*
+* 类数组转化数组
+* */
+var demo = {0: 'a', 1: 'b',2: 'c',length: 3};
+demo = Array.prototype.slice.call(demo);
+demo.forEach(function (key, index) {
+    console.log(key,index)
+})
+/*
+* 选择器，class,id,tagName
+* */
+function select(selector) {
+    var type = selector.substring(0,1);
+    console.log(type)
+    switch (type) {
+        case '.':
+            return document.getElementsByClassName(selector.substring(1))
+        case '#':
+            return document.getElementById(selector.substring(1))
+        default:
+            return document.getElementsByTagName(type)
+
+    }
+}
+select('div')
+select('#test')
+select('.real')
